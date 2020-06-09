1c933219595716204bf9eff0d939e342d6dabaa4
mohankapaganti <59202569+mohankapaganti@users.noreply.github.com>
2020-06-09 14:24:01 +0530

    changes done for mistakes

--- todo.rb
+++ todo.rb
@@ -2,6 +2,10 @@ require "date"
 
 class Todo < ActiveRecord::Base
 
+  def due_today?
+    due_date == Date.today
+  end
+
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
     display_date = due_today? ? nil : due_date
